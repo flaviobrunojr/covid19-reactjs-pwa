0c51bc4e6b80c76f38fa8f157caa46286a01f947
Flavio Bruno Junior <flavio@flavio-games.hinkelmann.com.br>
2020-07-09 12:43:28 -0300

    Primeiro commit covid19-pwa

---
+++ build/precache-manifest.e72b4bffd793c19b2ff25b4bc54baaa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ef4f67b69f693794fd64175a033384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e24b04ae5f81aa14892",
+    "url": "/static/js/2.518bafd1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.518bafd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e25270f0c99c96305b",
+    "url": "/static/js/main.7f787d2a.chunk.js"
+  },
+  {
+    "revision": "d4b80e953d3d5c2fa68d",
+    "url": "/static/js/runtime-main.7f91de96.js"
+  },
+  {
+    "revision": "ce09ee7114e1a82303167b3a544900ef",
+    "url": "/static/media/covid.ce09ee71.jpg"
+  },
+  {
+    "revision": "c98e1c22d40dcbd56d9d7f60b7a8a410",
+    "url": "/static/media/refresh.c98e1c22.svg"
+  }
+]);
